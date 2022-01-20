e05984f109048465dc623441b35b6d741b823bd3
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-01-20 23:47:36 +0100

    added map function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class SkylabArray {
   length;
   data = {};
-  
+
   constructor(...argumentos) {
     let index = 0;
     for (const argumento of argumentos) {
@@ -10,7 +10,7 @@ class SkylabArray {
     }
     this.length = index;
   }
-  
+
   some(condition) {
     let result = false;
     for (const i in this.data) {
@@ -20,11 +20,41 @@ class SkylabArray {
     }
     return result;
   }
-   push(...argumentos) {
+
+  push(...argumentos) {
     for (const valor of argumentos) {
       this[this.length] = valor;
       this.length++;
     }
   }
-}
 
+  filter(condition) {
+    const newArray = new SkylabArray();
+    for (const i in this.data) {
+      if (condition(this.data[i])) {
+        newArray.push(this.data[i]);
+      }
+    }
+    return newArray;
+  }
+
+  find(condition) {
+    for (const i in this.data) {
+      if (condition(this.data[i])) {
+        return this.data[i];
+      }
+    }
+    return null;
+  }
+
+  map(modifier) {
+    const newArray = new SkylabArray();
+    for (const i in this.data) {
+      if (Object.hasOwnProperty.call(this.data, i)) {
+        const element = this.data[i];
+        newArray.push(modifier(element));
+      }
+    }
+    return newArray;
+  }
+}
