2b193f5068cafafd7ade235ac50e19ba67773975
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-01-20 20:48:10 +0100

    added some method to class SkylabArray

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 class SkylabArray {
   length;
+  data = {};
   constructor(...argumentos) {
     let index = 0;
     for (const argumento of argumentos) {
-      this[index] = argumento;
+      this.data[index] = argumento;
       index++;
     }
     this.length = index;
   }
+
+  some(condition) {
+    let result = false;
+    for (const i in this.data) {
+      if (condition(this.data[i]) === true) {
+        result = true;
+      }
+    }
+    return result;
+  }
 }
 
-const prova = new SkylabArray("hola", false, 90);
-console.log(prova, prova.length);
+const testArray = new SkylabArray(9, 25);
+
+console.log(testArray);
+
+const even = (element) => element % 2 === 0;
+
+console.log(testArray.some(even));
